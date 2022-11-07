1accbc31114e2ba8896daf4682358e4dc44b0fb8
Bradley <gonzales.bradleyd@gmail.com>
2022-11-07 16:19:41 -0500

    Add finished work

--- knightsTravails.js
+++ knightsTravails.js
@@ -39,6 +39,16 @@ class Queue {
     return this.length === 0;
   }
 }
+class KnightNode {
+  constructor(value, predecessor = null, children = []) {
+    this.value = value;
+    this.predecessor = predecessor;
+    this.children = children;
+  }
+  set setPrecedessor(value) {
+    this.KnightNode.predecessor = value;
+  }
+}
 const Knight = () => {
   // const KNIGHT_OFFSETS = [
   //   [1, 2], [1, -2],
@@ -57,8 +67,6 @@ const Knight = () => {
     const possibleMoves = [];
     board[0][0] = '[0, 0]';
     board[start[0]][start[1]] = '𐂃';
-    console.log(board);
-    console.log(board[start[0]][start[1]]);
     possibleMoves.push([start[0] - 2, start[1] - 1]);
     possibleMoves.push([start[0] - 1, start[1] - 2]);
     possibleMoves.push([start[0] - 2, start[1] + 1]);
@@ -70,33 +78,47 @@ const Knight = () => {
     const legalMoves = possibleMoves.filter((move) => (move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7));
     return legalMoves;
   };
-  const visited = [];
-  const knightMoves = (start, end) => {
-    let adjacentList = generateMoves(start);
+  const bfs = (start) => {
     const queue = [];
-    queue.push([start[0], start[1]]);
-    console.log(adjacentList);
-    console.log(queue);
-    console.log(queue.some((element) => visited.includes(element)));
+    const visited = new Array().fill(false);
+    visited[start] = true;
+    const prev = new Array().fill(null);
+    queue.push(start);
     while (queue.length !== 0) {
-      const found = queue.some((r) => visited.includes(r));
-      if (found) {
-        queue.shift();
-      } else if (!found) {
-        visited.push(queue.shift());
+      const current = queue.shift();
+      const neighbors = generateMoves(current);
+      for (n of neighbors) {
+        if (!visited[n]) {
+          queue.push(n);
+          visited[n] = true;
+          prev[n] = current;
+        }
       }
-      while (adjacentList.length !== 0) {
-        queue.push(adjacentList.shift());
-      }
-      adjacentList = generateMoves(queue[0]);
     }
+    return prev;
+  };
+  const knightMoves = (start, end) => {
+    const prev = bfs(start);
+    return getPath(start, end, prev);
+  };
+  const getPath = (start, end, prev) => {
+    const path = [];
+    for (let at = end; at !== undefined; at = prev[at]) {
+      path.push(at);
+    }
+    path.reverse();
+    if (JSON.stringify(path[0]) === JSON.stringify(start)) {
+      console.log(`Finished in ${path.length - 1} moves` );
+      return path;
+    }
+    return [];
   };
   return {
-    newPiece, knightMoves, generateMoves,
+    newPiece, knightMoves, generateMoves, bfs, getPath,
   };
 };
 
 const test = Knight();
-console.log(makeBoard());
+makeBoard();
 test.newPiece();
-console.log(test.knightMoves([0, 0], [2, 1]));
+console.log(test.knightMoves([0, 0], [5, 3]));
